de2a6a2c4c1f03f1d2cc6e672761a2a57fb908e5
holly <holly.gummersonn@gmail.com>
2026-03-23 12:42:37 -0700

    added spectral-representations module

---
+++ src/core_io/spectral_representations.rs
@@ -0,0 +1,29 @@
+// https://librosa.org/doc/latest/core.html#spectral-representations
+// use num_complex::Complex;
+// use ndarray::ArrayD;
+// use ndarray::Axis;
+// use ndarray::IxDyn;
+
+
+// ---------------------------------------------------------------------------------------
+// STFT -> short time fourier transform, represents a signal in the time-frequency domain
+// by computing dft over short overlapping windows
+// parameters: 
+// returns: 
+// ---------------------------------------------------------------------------------------
+pub fn stft() { unimplemented!() }
+
+// ---------------------------------------------------------------------------------------
+// ISTFT -> inverse short time fourier transform, reconstructs a time-domain signal from its stft
+// parameters:
+// returns:
+// ---------------------------------------------------------------------------------------
+pub fn istft() { unimplemented!() }
+
+// ---------------------------------------------------------------------------------------
+// REASSIGNED_SPECTROGRAM -> a technique for improving the time-frequency resolution of a 
+// spectrogram by reassigning energy to more accurate time-frequency locations
+// parameters:
+// returns:
+// ---------------------------------------------------------------------------------------
+pub fn reassigned_spectrogram() { unimplemented!() }
