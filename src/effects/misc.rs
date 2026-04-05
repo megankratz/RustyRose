7ff486d36326284c894957ef722e77a984618cc6
Alex <alexanderw@uvic.ca>
2026-04-05 00:32:23 -0700

    added more effects functions

--- src/effects/misc.rs
+++ src/effects/misc.rs
@@ -1,5 +1,5 @@
-use ndarray::{Array1, Array2, Axis, s, array};
-use crate::{core_io::*, feature::rms, utilities::frame};
+use ndarray::{Array1, Array2, ArrayD, Axis, s, array, concatenate, arr1};
+use crate::{core_io::*, feature::rms};
 
 pub fn trim(
     y : &Array1<f64>,
@@ -29,6 +29,95 @@ pub fn trim(
 
     (y.slice(s![start..end]).to_owned(), Array1::from(array![start, end]))
 }
+
+
+pub fn split(
+    y : &Array1<f64>,
+    top_db: Option<f64>,
+    ref_ : f64,
+    frame_length : Option<i64>,
+    hop_length : Option<i32>
+) -> Array2<i32> {
+
+    let non_silent : Array1<bool> = signal_to_frame_nonsilent(y, frame_length, hop_length, top_db.unwrap_or(60.), ref_);
+
+    // Get all indices where frames change between silent and non-silent
+    let mut edges : Array1<usize> = non_silent
+        .iter()
+        .zip(non_silent.iter().skip(1))
+        .enumerate()
+        .filter_map(|(i, (&a, &b))| {
+            if a != b {
+                Some(i + 1) // add 1 since we're not counting first frame
+            } else {
+                None
+            }
+        }).collect();
+
+    // If the first frame had high energy, count it
+    if non_silent[0]{
+        edges = concatenate![Axis(0), arr1(&[0]), edges.view()];
+    }
+
+    // Same for the last frame
+    if *non_silent.last().unwrap_or(&false) {
+        edges = concatenate![Axis(0), edges.view(), arr1(&[non_silent.len()])];
+    }
+
+    // Convert from frames to samples
+    let samples : Array1<i32> = edges
+        .iter()
+        .map(|&x |{ 
+            let i = frames_to_samples(x as i32, hop_length, None);
+            i.min(y.len() as i32) //  ensure each sample is in the input array
+        })
+        .collect();
+
+    let len = samples.len();
+    samples 
+        .into_shape((len / 2, 2))
+        .unwrap()
+}
+
+
+pub fn remix(
+    y : Array1<f64>,
+    intervals: Array2<usize>,
+    align_zeros : Option<bool>
+) -> Array1<f64>{
+
+    let mut out : Vec<f64> = Vec::new();
+
+    let zeros : Vec<usize> = if align_zeros.unwrap_or(true) {
+        let y_d: ArrayD<f32> = y.mapv(|x| x as f32).into_dyn(); // convert y to right format
+        let crossings : Array1<bool>  = zero_crossing(&y_d, 1e-10, None, true, true, 0)
+            .into_dimensionality::<ndarray::Ix1>().unwrap();
+        
+        let mut indices : Vec<usize> = crossings
+            .indexed_iter()
+            .filter_map(|(i, &v)| if v {Some(i)} else {None})
+            .collect();
+        indices.push(y.len());
+        indices
+    } else {
+        Vec::new()
+    };
+
+
+    for row in intervals.rows() {
+        let start : usize = row[0];
+        let end : usize = row[1];
+        if align_zeros.unwrap_or(true) {
+            // TODO: Implement match_events
+        }        
+        out.extend(y
+            .slice(s![start..end])
+            .iter());
+    }
+
+    Array1::from(out)
+}
+
 
 
 fn signal_to_frame_nonsilent(
